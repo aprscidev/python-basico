8ec75c5fb6d0885c2a2579bafdffee274095b4de
Ana Ribeiro <anascidev@outlook.com>
2024-01-27 22:12:07 -0300

    Exercício 3 da lista de exercícios do curso de Python Básico da Womakerscode.

---
+++ exercicios/exercicio3.py
@@ -0,0 +1,17 @@
+#Exercício 3 - Conceitos Básicos - Womakerscode
+
+#Solicitando a distância em quilômetros
+km = int(input('Digite a distância em km: '))
+
+#Conversões
+m = km * 1000
+cm = km * 100000
+mm = km * 1000000
+
+#Saídas
+print(f'{km} quilômetros é igual a {m} metros.')
+print(f'{km} quilômetros é igual a {cm} centímetros.')
+print(f'{km} quilômetros é igual a {mm} milímetros.')
+
+#Finalizando
+print('---')
