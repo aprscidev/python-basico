fee0fbbed80842eb838d3696a76ef4b8249e8999
Ana Ribeiro <anascidev@outlook.com>
2024-01-28 20:45:28 -0300

    Exercício 9 da lista de exercícios do curso Python Básico Womakerscode

---
+++ exercicios/exercicio9.py
@@ -0,0 +1,13 @@
+#Exercício 9 - Conceitos Básicos - Womakerscode
+
+#Solicitando informações ao usuário
+nome = input('Digite o seu nome: ')
+idade = int(input('Digite sua idade: '))
+cidade = input('Digite sua cidade: ')
+profissao = input('Digite sua profissão: ')
+
+#Saídas
+print(f'Olá {nome}, prazer te conhecer! Sou de {cidade} também e estou migrando de área.')
+
+#Finalização
+print('---')
