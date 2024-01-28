4a990abec11588a4b9e885b1bf1ad5b41c9a4adf
Ana Ribeiro <anascidev@outlook.com>
2024-01-28 10:16:18 -0300

    Exercício 4 da lista de exercícios de Python Básico Womakerscode

---
+++ exercicios/exercicio4.py
@@ -0,0 +1,15 @@
+#Exercício 4 - Conceitos Básicos - Womakerscode
+
+#Solicitando a quantidade de litros de combustível e distância percorrida
+litros_combustivel = int(input('Digite a quantidade de combustível em litros: '))
+distancia = int(input('Digite a distância percorrida em quilômetros: '))
+
+#Calculando o consumo médio
+consumo_medio = distancia / litros_combustivel
+
+#Saida
+print(f'O consumo médio é de {consumo_medio} km/l.')
+
+#Finalizando
+print('---')
+
