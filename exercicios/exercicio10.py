433a3eac68fad432e5093470def5b93acc12b652
aprscidev <anascidev@outlook.com>
2024-01-30 08:55:08 -0300

    Exercicio 10 da lista de exercícios de Python Básico

---
+++ exercicios/exercicio10.py
@@ -0,0 +1,14 @@
+#Exercício 1 - Tomada de decisão - Womakerscode
+
+#Solicitando os números para o usuário
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+#Tomada de decisão
+if num1 > num2:
+    print(f'O maior número é: {num1}.')
+else:
+    print(f'O maior número é: {num2}')
+
+#Finalizando
+print('---')
