013c38f1332ddc035dd022c471132ec256dbe02e
aprscidev <anascidev@outlook.com>
2024-01-31 15:50:01 -0300

    Exercício 21 da lista de exercícios de Python Básico

--- exercicios/exercício21.py
+++ exercicios/exercício21.py
@@ -1,7 +1,11 @@
 #Exercício 1 - Funções - Womakerscode
 
+#Função para calcular a soma dos números
 def soma(n1, n2, n3):
     print(f'A soma dos três números informados é: {n1+n2+n3}.')
 
-
+#Chamada da função com os números escolhidos
 soma(4, 5, 6)
+
+#Finalização
+print('---')
