ab3284ea13ce34431c48da13757fb685c37e7910
aprscidev <anascidev@outlook.com>
2024-01-31 14:55:56 -0300

    Exercício 22 da lista de exercícios de Python Básico

---
+++ exercicios/exercicio22.py
@@ -0,0 +1,7 @@
+#Exercício 2 - Funções - Womakerscode
+
+def inverso(num1):
+    inv_num = int(str(num1)[::-1])
+    print(f'Você digitou o valor {num1}. O inverso dele é {inv_num}')
+
+inverso(987)
